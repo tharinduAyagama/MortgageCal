bea28a9f45d3964fbf6587fdf365bb3f531b8731
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-08-30 22:21:57 +0530

    add the remaining balance shedule

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Main {
 
+    final static byte MONTH_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
     public static void main(String[] args) {
 
         int priciple = (int)readNumber("Principle: " , 1000 , 1000000);
@@ -12,7 +15,15 @@ public class Main {
         int period = (int)readNumber("Period(Years): " , 1,30);
 
         double mortgage = calculateMortgage(priciple,annualInterstRate,period);
+        System.out.println("--MORTGAGE--");
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        System.out.println();
+
+        System.out.println("--REMAINING PAYMENT SHEDULE--");
+        for (int i = 0; i <= period*MONTH_IN_YEAR; i++) {
+            System.out.println("The remaining payment after paying " + i +" time is " + calculateRemainingPayment(priciple,annualInterstRate,period,i));
+
+        }
     }
 
     public static double readNumber(String prompt , double min , double max){
@@ -28,11 +39,14 @@ public class Main {
         }
         return value;
     }
+    public static double calculateRemainingPayment(int priciple , float annualInterstRate , int period ,int numberOfPaymentsDone){
+        float monthlyInterestRate = annualInterstRate/(MONTH_IN_YEAR*PERCENT);
+        int numberOfPayments = period*12;
+            double remaininePayment = priciple * (Math.pow((1+monthlyInterestRate) , numberOfPayments) - Math.pow((1+monthlyInterestRate) , numberOfPaymentsDone)) / (Math.pow((1+monthlyInterestRate) , numberOfPayments) -1);
+            return remaininePayment;
+    }
 
     public static double calculateMortgage(int priciple , float annualInterstRate , int period){
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         float monthlyInterestRate = annualInterstRate/(MONTH_IN_YEAR*PERCENT);
         int numberOfPayments = period*12;
 
